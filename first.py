239fbb20b7d8ad0402e1213974c2db362dc0fe85
rameshMonika <rameshmonika11@gmail.com>
2024-03-12 00:30:03 +0800

    updates

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -14,6 +14,15 @@ def index():
 @app.route('/about')
 def about():
     return render_template("/about.html",name=name,age=age,qualification=qualification,li=list)
+
+
+@app.route('/',methods=['POST'])
+def getdata():
+   name=request.form['name']
+   age=request.form['age']
+   db=request.form['dateofbirth']
+   return render_template('pass.html',n=name,a=age,d=db)
+
 
 if __name__=="__main__":
     app.debug = True
